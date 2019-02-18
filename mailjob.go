64200c9061dfc3229e5abc6cb5b23b966712d952
James Magahern <james@magahern.com>
2019-02-18 00:12:25 -0800

    Project cleanup
    Tabs to spaces
    Remove VSCode directory
    Remove unused import

--- mailjob.go
+++ mailjob.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "fmt"
-	"net/smtp"
-	"os"
-	"time"
+    "net/smtp"
+    "os"
+    "time"
 )
 
 // RepeatType defines how often the job is repeated. Defaults to None
@@ -12,74 +12,74 @@ type RepeatType uint
 
 // RepeatType options
 const (
-	RepeatNone RepeatType = iota
-	RepeatDaily
-	RepeatWeekly
-	RepeatMonthly
+    RepeatNone RepeatType = iota
+    RepeatDaily
+    RepeatWeekly
+    RepeatMonthly
 )
 
 func (r RepeatType) String() string {
-	switch r {
-	case RepeatNone:
-		return "None"
-	case RepeatDaily:
-		return "Daily"
-	case RepeatWeekly:
-		return "Weekly"
-	case RepeatMonthly:
-		return "Monthly"
-	}
+    switch r {
+    case RepeatNone:
+        return "None"
+    case RepeatDaily:
+        return "Daily"
+    case RepeatWeekly:
+        return "Weekly"
+    case RepeatMonthly:
+        return "Monthly"
+    }
 
-	return "?"
+    return "?"
 }
 
 // MailJob Represents a mail message to be sent at `date`
 type MailJob struct {
-	Date     time.Time
-	Message  string
-	FilePath string
-	Repeat   RepeatType
+    Date     time.Time
+    Message  string
+    FilePath string
+    Repeat   RepeatType
 }
 
 func (j MailJob) String() string {
-	str := "{\n"
-	str += "\t At: " + j.Date.String() + "\n"
-	str += "\t Msg: " + j.Message + "\n"
-	str += "\t File: " + j.FilePath + "\n"
-	str += "\t Repeats: " + j.Repeat.String() + "\n"
+    str := "{\n"
+    str += "\t At: " + j.Date.String() + "\n"
+    str += "\t Msg: " + j.Message + "\n"
+    str += "\t File: " + j.FilePath + "\n"
+    str += "\t Repeats: " + j.Repeat.String() + "\n"
 
-	str += "}"
+    str += "}"
 
-	return str
+    return str
 }
 
 // Execute executes the mailjob using sendmail
 func (j MailJob) ExecuteWithConfig(config Config) error {
-	auth := smtp.PlainAuth(
-		"",                          // identity
-		config.Server.Username,      // username
-		config.Server.Password,      // password
-		config.Server.Host,          // host
-	)
+    auth := smtp.PlainAuth(
+        "",                          // identity
+        config.Server.Username,      // username
+        config.Server.Password,      // password
+        config.Server.Host,          // host
+    )
 
-	msgString := "To: " + config.Email.To + "\r\n"
-	msgString += "From: " + config.Email.From + "\r\n"
-	msgString += "Subject: [E-Reminder] " + j.Message + "\r\n\r\n"
-	msgString += j.Message
+    msgString := "To: " + config.Email.To + "\r\n"
+    msgString += "From: " + config.Email.From + "\r\n"
+    msgString += "Subject: [E-Reminder] " + j.Message + "\r\n\r\n"
+    msgString += j.Message
 
-	err := smtp.SendMail(
+    err := smtp.SendMail(
         fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port),
-		auth,
+        auth,
         config.Email.From,
-		[]string{config.Email.To},
-		[]byte(msgString),
-	)
+        []string{config.Email.To},
+        []byte(msgString),
+    )
 
-	return err
+    return err
 }
 
 // SelfDestruct deletes the file representation of the mail job
 func (j MailJob) SelfDestruct() {
-	os.Remove(j.FilePath)
+    os.Remove(j.FilePath)
 }
 
--- pipe.go
+++ pipe.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
-	"os"
-	"path/filepath"
-	"syscall"
+    "bytes"
+    "io"
+    "log"
+    "os"
+    "path/filepath"
+    "syscall"
 )
 
 const pipesDirectory string = "/tmp/ereminders_pipes"
@@ -14,75 +14,75 @@ const commandPipeName string = "commands"
 const commandResultPipeName string = "recv"
 
 func getTemporaryDirectory() (string, error) {
-	err := os.MkdirAll(pipesDirectory, 0755)
-	if err != nil {
-		log.Println("Error: Unable to create temporary directory")
-		return "", err
-	}
+    err := os.MkdirAll(pipesDirectory, 0755)
+    if err != nil {
+        log.Println("Error: Unable to create temporary directory")
+        return "", err
+    }
 
-	return pipesDirectory, nil
+    return pipesDirectory, nil
 }
 
 func makeNamedPipe(name string) string {
     tempDir, _ := getTemporaryDirectory()
-	namedPipeFilename := filepath.Join(tempDir, name)
-	syscall.Mkfifo(namedPipeFilename, 0600)
+    namedPipeFilename := filepath.Join(tempDir, name)
+    syscall.Mkfifo(namedPipeFilename, 0600)
 
-	return namedPipeFilename
+    return namedPipeFilename
 }
 
 func openPipe(pipeName string, flags int) *os.File {
-	filename := makeNamedPipe(pipeName)
-	pipe, err := os.OpenFile(filename, flags, 0600)
-	if err != nil {
-		log.Print("Error opening pipe: ")
-		log.Println(err)
-		return nil
-	}
+    filename := makeNamedPipe(pipeName)
+    pipe, err := os.OpenFile(filename, flags, 0600)
+    if err != nil {
+        log.Print("Error opening pipe: ")
+        log.Println(err)
+        return nil
+    }
 
-	return pipe
+    return pipe
 }
 
 // ReadCommand blocks on the command pipe until data is received
 func ReadCommand() string {
-	commandPipe := openPipe(commandPipeName, os.O_RDONLY)
+    commandPipe := openPipe(commandPipeName, os.O_RDONLY)
 
-	var commandBuf bytes.Buffer
-	io.Copy(&commandBuf, commandPipe)
-	commandPipe.Close()
+    var commandBuf bytes.Buffer
+    io.Copy(&commandBuf, commandPipe)
+    commandPipe.Close()
 
-	return commandBuf.String()
+    return commandBuf.String()
 }
 
 // SendCommand sends a command string to the command named pipe
 func SendCommand(command string) {
-	commandPipe := openPipe(commandPipeName, os.O_WRONLY)
-	commandPipe.WriteString(command)
-	commandPipe.Close()
+    commandPipe := openPipe(commandPipeName, os.O_WRONLY)
+    commandPipe.WriteString(command)
+    commandPipe.Close()
 }
 
 // SendResponse sends the response to the tx/rx pipe
 func SendResponse(response string) {
-	txPipe := openPipe(commandResultPipeName, os.O_WRONLY)
-	txPipe.WriteString(response)
-	txPipe.Close()
+    txPipe := openPipe(commandResultPipeName, os.O_WRONLY)
+    txPipe.WriteString(response)
+    txPipe.Close()
 }
 
 // ReadResponse blocks while it waits for a response on the tx/rx named pipe
 func ReadResponse() string {
-	rxPipe := openPipe(commandResultPipeName, os.O_RDONLY)
+    rxPipe := openPipe(commandResultPipeName, os.O_RDONLY)
 
-	var rxBuf bytes.Buffer
-	io.Copy(&rxBuf, rxPipe)
-	rxPipe.Close()
+    var rxBuf bytes.Buffer
+    io.Copy(&rxBuf, rxPipe)
+    rxPipe.Close()
 
-	return rxBuf.String()
+    return rxBuf.String()
 }
 
 // TransmitCommand sends the command to the command pipe, then waits for a response from the
 // recv pipe
 func TransmitCommand(command string) string {
-	SendCommand(command)
-	return ReadResponse()
+    SendCommand(command)
+    return ReadResponse()
 }
 
